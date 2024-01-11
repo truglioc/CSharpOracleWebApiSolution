4f7498d44853099e7ac1a828372e9be7813ecdd1
truglioc <103527848+truglioc@users.noreply.github.com>
2024-01-11 10:47:19 -0600

    Add project files.

---
+++ DemoLibrary/Class2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public static class ApiHelper
+    {
+        public static HttpClient ApiClient { get; set; } 
+        public static void InitializeClient()
+        {
+            ApiClient = new HttpClient();
+            ApiClient.BaseAddress = new Uri("https://fa-esie-saasfaprod1.fa.ocs.oraclecloud.com/");
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
